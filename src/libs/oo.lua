64aa83ef1247adb833f468aeb6a14d78d6d731c1
robinafro <oble.ro.2023@skola.ssps.cz>
2024-07-18 17:51:29 +0200

    removed bad code

--- src/libs/oo.lua
+++ src/libs/oo.lua
@@ -2,56 +2,16 @@
 
 local oo = {}
 
--- function oo.class(...)
---     local proto = oo.aug({}, ...)
---     proto.__index = proto
-
---     function proto.new(...)
---         local self = setmetatable({}, proto)
-
---         if proto.init then
---             self:init(...)
---         end
---         return self
---     end
-
---     return proto
--- end
-
--- function oo.aug(target, ...)
---     for _, t in ipairs({ ... }) do
---         for k, v in pairs(t) do
---             target[k] = v
---         end
---     end
---     return target
--- end
-
--- return oo
-
-local oo = {}
-
 function oo.class(...)
-    local bases = { ... }
-    local proto = {}
-
-    for _, base in ipairs(bases) do
-        for k, v in pairs(base) do
-            if k ~= "init" then -- Avoid copying the constructor
-                proto[k] = v
-            end
-        end
-    end
-
+    local proto = oo.aug({}, ...)
     proto.__index = proto
 
     function proto.new(...)
         local self = setmetatable({}, proto)
 
-        if self.init then
+        if proto.init then
             self:init(...)
         end
-
         return self
     end
 
