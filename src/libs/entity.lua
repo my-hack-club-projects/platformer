51a769e29a3520ab139b3c75bd0872c873c300a0
robinafro <oble.ro.2023@skola.ssps.cz>
2024-07-18 16:18:29 +0200

    Entity class

---
+++ src/libs/entity.lua
@@ -0,0 +1,43 @@
+local oo = require 'libs.oo'
+local Vector2 = require 'types.Vector2'
+
+local Entity = oo.class()
+
+function Entity:init()
+    self.position = Vector2(0, 0)
+    self.size = Vector2(0, 0)
+    self.rotation = 0
+end
+
+function Entity:add(component)
+    table.insert(self.components, component)
+end
+
+function Entity:update(dt)
+    -- This function is to be overridden by classes that inherit from Entity.
+end
+
+function Entity:draw_setup(unitSize)
+    -- This function translates and rotates the screen so that the entity can be drawn
+    love.graphics.translate(self.position.x * unitSize, self.position.y * unitSize)
+    love.graphics.rotate(-self.rotation)
+end
+
+function Entity:draw(dt, unitSize)
+    -- This function draws a rectangle at the entity's position.
+    love.graphics.push()
+
+    self.draw_setup(unitSize)
+
+    love.graphics.rectangle(
+        'fill',
+        -self.size.x * unitSize / 2,
+        -self.size.y * unitSize / 2,
+        self.size.x * unitSize,
+        self.size.y * unitSize
+    )
+
+    love.graphics.pop()
+end
+
+return Entity
