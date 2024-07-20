28767dff0b0d950bcda6a2938e77f019c9c7ca3d
robinafro <oble.ro.2023@skola.ssps.cz>
2024-07-20 10:29:07 +0200

    remove old code

--- src/game/states/Play/state.lua
+++ src/game/states/Play/state.lua
@@ -31,16 +31,10 @@ function PlayState:enter()
     self.player = self.entity.new(Player)
     self.player.gravity = self.game.Gravity
 
-    -- self.test = self.entity.new(Entity, "Test")
-    -- self.test.position = Vector2(0, -3)
-    -- self.test.size = Vector2(10, 1)
-    -- self.test.anchored = true
-
     self.map = Map(self.game)
     self.map:generate()
 
     for _, pad in ipairs(self.map.pads) do
-        print(pad.position, pad.size)
         self.entity.insert(pad)
     end
 end
